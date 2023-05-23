03ea44e636af3ea90a566e1ba34c0167f2c2aa95
Bruno Gorosito <gorositobrunoalejandro@gmail.com>
2023-05-23 16:19:22 -0300

    conectado a bd

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -1,8 +1,14 @@
 
+import clientPromise from '@/lib/mongodb';
 import { NextResponse } from 'next/server';
  
-export async function GET() {
-  
+export async function GET(req, res) {
+  const mongoClient = await clientPromise;
+  const data = await mongoClient
+    .db()
+    .collection('tareas')
+    .find()
+    .toArray()
  
-  return NextResponse.json({ msg: 'hola' });
+  return NextResponse.json({tasks: JSON.parse(JSON.stringify(data))});
 }
