b21271cb0791255048275b2b728ed7379f40b1f7
Bruno Gorosito <gorositobrunoalejandro@gmail.com>
2023-05-28 16:11:56 -0300

    con delete and update

--- app/components/Card.js
+++ app/components/Card.js
@@ -25,14 +25,16 @@ const Card = ({task}) => {
         <div 
             className="flex w-100 justify-between mt-4 border border-gray-600 border-solid p-3 rounded-md items-center">
             <p>{task.title}</p>
-            <button
-                className="rounded-md px-3 py-1 bg-red-500 text-white capitalize select-none"
-                onClick={deleteTask}
-            >Eliminar</button>
-            <button
-                onClick={handleStatus}
-                className={`rounded-md px-3 select-none text-white py-1 capitalize ${task.status === 'incompleted' ? "bg-red-500" : "bg-green-500"}`}
-            >{task.status}</button>
+            <div>
+                <button
+                    className="rounded-md px-3 py-1 bg-red-500 bg-c text-white capitalize select-none mx-2"
+                    onClick={deleteTask}
+                >Eliminar</button>
+                <button
+                    onClick={handleStatus}
+                    className={`rounded-md w-36 px-3 select-none text-white py-1 capitalize mx-2 bg-opacity-75 ${task.status === 'incompleted' ? "bg-red-500" : "bg-green-500"}`}
+                >{task.status}</button>
+            </div>
         </div>
     )
 }
